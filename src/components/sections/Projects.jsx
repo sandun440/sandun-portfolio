83a4d8597ee24110179cbfdf00512e7172164f1b
Sandun Sameera <sameerasandun440@gmail.com>
2026-01-06 15:28:07 +0530

    add project to portfolio

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -18,15 +18,26 @@ export const Projects = () => {
             <div className="relative overflow-hidden group p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               {/* Shine effect overlay */}
               <div className="absolute inset-0 -left-full group-hover:left-full bg-gradient-to-r from-transparent via-white/20 to-transparent w-full h-full transition-all duration-2000 ease-out pointer-events-none" />
-              
-              <h3 className="text-xl font-bold mb-2">Crystal Beauty Clare Web App</h3>
+
+              <h3 className="text-xl font-bold mb-2">
+                Crystal Beauty Clare Web App
+              </h3>
               <p className="text-gray-400 mb-4">
-                A full-stack web application designed for Crystal Beauty Clare — a modern beauty service platform. 
-                It offers an intuitive interface for customers to explore services, buy products and manage profiles.
+                A full-stack web application designed for Crystal Beauty Clare —
+                a modern beauty service platform. It offers an intuitive
+                interface for customers to explore services, buy products and
+                manage profiles.
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Vite", "Tailwind CSS", "JavaScript", "Express.js", "MongoDB"].map((tech, key) => (
+                {[
+                  "React",
+                  "Vite",
+                  "Tailwind CSS",
+                  "JavaScript",
+                  "Express.js",
+                  "MongoDB",
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -37,7 +48,6 @@ export const Projects = () => {
               </div>
 
               <div className="flex flex-wrap gap-6 my-4">
-                
                 <a
                   href="https://github.com/sandun440/cbc-frontend.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors flex items-center gap-1 w-full sm:w-auto justify-center sm:justify-start"
@@ -71,10 +81,11 @@ export const Projects = () => {
             <div className="relative overflow-hidden group p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               {/* Shine effect overlay */}
               <div className="absolute inset-0 -left-full group-hover:left-full bg-gradient-to-r from-transparent via-white/20 to-transparent w-full h-full transition-all duration-2000 ease-out pointer-events-none" />
-              
+
               <h3 className="text-xl font-bold mb-2">Movie Finder</h3>
               <p className="text-gray-400 mb-4">
-                A HTML/CSS-based movie search application that lets users find movies, view details, and explore trending films.
+                A HTML/CSS-based movie search application that lets users find
+                movies, view details, and explore trending films.
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
@@ -106,7 +117,57 @@ export const Projects = () => {
                 >
                   Live Demo →
                 </a>
-                
+              </div>
+            </div>
+
+            {/* University Project */}
+            <div className="relative overflow-hidden group p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              {/* Shine effect overlay */}
+              <div className="absolute inset-0 -left-full group-hover:left-full bg-gradient-to-r from-transparent via-white/20 to-transparent w-full h-full transition-all duration-2000 ease-out pointer-events-none" />
+
+              <h3 className="text-xl font-bold mb-2">
+                University project - Environment pollution
+              </h3>
+              <p className="text-gray-400 mb-4">
+                A Simple HTML, CSS, and JavaScript website project focused on
+                raising awareness about environmental pollution and promoting
+                eco-friendly practices.
+              </p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                {[
+                  "HTML",
+                  "CSS",
+                  "JavaScript",
+                ].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex flex-wrap gap-6 my-4">
+                <a
+                  href="https://github.com/sandun440/Environment_pollution.git"
+                  className="text-blue-400 hover:text-blue-300 transition-colors flex items-center gap-1 w-full sm:w-auto justify-center sm:justify-start"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub className="opacity-70 text-lg" />
+                  Project on Github
+                </a>
+
+                <a
+                  href="https://sandun440.github.io/Environment_pollution/"
+                  className="relative text-blue-400 hover:text-blue-300 transition-colors w-full sm:w-auto text-center inline-block after:content-[''] after:absolute after:w-0 after:h-[2px] after:left-0 after:bottom-0 after:bg-blue-400 hover:after:w-full after:transition-all after:duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Live Demo →
+                </a>
               </div>
             </div>
           </div>
